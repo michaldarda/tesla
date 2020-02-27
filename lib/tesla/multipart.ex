add606fdc3d8c8d54ae94999c432933ddf0dff55
Michal Darda <mdarda@pm.me>
2020-02-27 23:38:25 +0100

    Make it more normal

--- lib/tesla/multipart.ex
+++ lib/tesla/multipart.ex
@@ -1,4 +1,6 @@
 defmodule Tesla.Multipart do
+  @boundary_length 40
+
   @moduledoc """
   Multipart functionality.
 
@@ -52,7 +54,7 @@ defmodule Tesla.Multipart do
   """
   @spec new() :: t
   def new do
-    %__MODULE__{boundary: unique_string()}
+    %__MODULE__{boundary: unique_string() |> String.pad_leading(@boundary_length, "-")}
   end
 
   @doc """
@@ -134,7 +136,7 @@ defmodule Tesla.Multipart do
   @spec body(t) :: part_stream
   def body(%__MODULE__{boundary: boundary, parts: parts}) do
     part_streams = Enum.map(parts, &part_as_stream(&1, boundary))
-    Stream.concat(part_streams ++ [["--#{boundary}--\r\n"]])
+    Stream.concat(part_streams ++ [["#{boundary}--\r\n"]])
   end
 
   @doc false
@@ -153,7 +155,7 @@ defmodule Tesla.Multipart do
       end
 
     Stream.concat([
-      ["--#{boundary}\r\n"],
+      ["--#{boundary}--\r\n"],
       part_headers,
       ["\r\n"],
       enum_body,
